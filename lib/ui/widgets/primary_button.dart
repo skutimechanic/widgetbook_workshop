5f5178b7590713c6e199fd93e565c1eb22dfb64a
jkoszowski <jakub.koszowski@thecodebrothers.pl>
2024-09-03 16:44:04 +0200

    feat: add button usecase

--- lib/ui/widgets/primary_button.dart
+++ lib/ui/widgets/primary_button.dart
@@ -43,11 +43,13 @@ class PrimaryButton extends StatelessWidget {
                   ),
                 ],
               ),
-            Text(
-              content,
-              style: AppTheme.of(context).typography.label.copyWith(
-                    color: AppTheme.of(context).text.inverse,
-                  ),
+            Expanded(
+              child: Text(
+                content,
+                style: AppTheme.of(context).typography.label.copyWith(
+                      color: AppTheme.of(context).text.inverse,
+                    ),
+              ),
             ),
             if (trailing != null)
               Row(
--- widgetbook/lib/ui/widgets/primary_button.dart
+++ widgetbook/lib/ui/widgets/primary_button.dart
@@ -7,3 +7,17 @@ import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
 /// Add Widgetbook use-case here
+
+@UseCase(
+  name: 'Default',
+  type: PrimaryButton,
+  // designLink:
+  //     'https://www.figma.com/design/HsANkdhbsCNTkXBzNJRNLD/Groceries-Demo?node-id=7337-3250&t=N6qwmLP7MP59ClWB-4',
+)
+Widget buildPrimaryButtonUseCase(BuildContext context) {
+  return PrimaryButton(
+    content:
+        context.knobs.string(label: 'Primary Button', initialValue: 'Click me'),
+    onPressed: () {},
+  );
+}
